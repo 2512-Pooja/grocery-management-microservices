a0008f8e475f7d72daa527aa84bdd6591fb359b4
Pooja <2512.poojasoni@gmail.com>
2025-09-07 14:22:21 +0530

    Temp changes

---
+++ user-service/src/main/java/com/grocery/user_service/entity/User.java
@@ -0,0 +1,119 @@
+package com.grocery.user_service.entity;
+
+import java.util.List;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Document(collection = "users")
+public class User {
+    @Id
+    private String userId;
+    @NotNull
+    private String firstName;
+    @NotNull
+    private String lastName;
+    @NotNull
+    private String userName;
+    @NotNull
+    @Email
+    private String email;
+    @NotNull
+    private String password;
+    @NotNull
+    @Size(max = 15, min = 10, message = "Phone number must not exceed 15 digits")
+    private String phoneNo;
+    @NotNull
+    private String status;
+    private List<String> orderIds;
+    @NotNull
+    private String role;
+
+    public User() {
+    }
+    public User(String userId, String firstName, String lastName, String userName, String email, String password,
+            String phoneNo, String status, List<String> orderIds, String role) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.phoneNo = phoneNo;
+        this.status = status;
+        this.orderIds = orderIds;
+        this.role = role;
+    }
+    public String getUserId() {
+        return userId;
+    }
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public List<String> getOrderIds() {
+        return orderIds;
+    }
+    public void setOrderIds(List<String> orderIds) {
+        this.orderIds = orderIds;
+    }
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User [userId=" + userId + ", firstName=" + firstName + ", lastName=" + lastName
+                + ", userName=" + userName + ", email=" + email + ", password=" + password + ", phoneNo=" + phoneNo
+                + ", status=" + status + ", orderIds=" + orderIds + ", role=" + role + "]";
+    }
+
+
+}
